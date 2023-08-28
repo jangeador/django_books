9b3f955b17c5e784bf4f26041696f46f2206989c
Delio Castillo <jangeador@gmail.com>
2023-08-27 20:02:55 -0700

    adds front end app

---
+++ next-books/app/page.tsx
@@ -0,0 +1,8 @@
+import Image from 'next/image'
+import {mapIncludes} from "yaml/dist/compose/util-map-includes";
+
+export default function Home() {
+    return (
+        <section>Main Section</section>
+    )
+}
