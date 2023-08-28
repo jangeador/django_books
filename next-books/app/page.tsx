4a115d79530cf5bc67a83999c5ddcf247d4c8484
Delio Castillo <jangeador@gmail.com>
2023-08-27 21:17:06 -0700

    adds photos

--- next-books/app/page.tsx
+++ next-books/app/page.tsx
@@ -1,8 +1,43 @@
-import Image from 'next/image'
-import {mapIncludes} from "yaml/dist/compose/util-map-includes";
+import Image from "next/image";
 
-export default function Home() {
+interface Book {
+    title: string;
+    pages: number;
+    author: { name: string };
+    country: string;
+    language: string;
+    image_link: string;
+    year: string;
+    link: string;
+}
+
+export default async function Home() {
+
+    const getBooks = async () => {
+        const res = await fetch('http://127.0.0.1:8000/api/v1/books/', {
+            headers: {"Content-type": "application/json"}
+        })
+        return await res.json()
+    }
+
+    const books = await getBooks()
+
     return (
-        <section>Main Section</section>
+        <section className="p-4">
+            <h1 className="text-xl pb-3">Books</h1>
+
+            {books.map((book: Book, index: number) => (
+                <div key={index} className="py-3 flex gap-2">
+                    <Image src={`/${book.image_link}`} alt={book.title}
+                    width={60} height={60}></Image>
+                    <div>
+                        <p className="font-bold">{book.title}</p>
+                        <p className="text-sm">{book.author.name}</p>
+                    </div>
+
+                </div>
+            ))}
+
+        </section>
     )
 }
